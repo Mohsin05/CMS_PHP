13f4dd2e06edfbab6ef0870a128c563911ecec34
mohsin05 <mohsinyounas05@gmail.com>
2017-03-01 20:42:56 +0100

    Update

--- index.php
+++ index.php
@@ -27,10 +27,7 @@
 
 
                ?>
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+
                 <!-- First Blog Post -->
                 <h2>
                     <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
